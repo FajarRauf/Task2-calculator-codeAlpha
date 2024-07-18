a123e102b487857c789a3c6859f44bc1700780db
FajarRauf <145549792+FajarRauf@users.noreply.github.com>
2024-07-18 19:28:55 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+const display = document.getElementById('display');
+
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+
+function calculate() {
+  try {
+    const result = eval(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+
+function clearDisplay() {
+  display.value = '';
+}
